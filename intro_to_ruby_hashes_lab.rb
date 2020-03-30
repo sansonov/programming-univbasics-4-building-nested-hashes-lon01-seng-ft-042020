2ec00f4356c831e1b5f1df3e61c8ce73ad8a9a72
Adao Natalino <sansonov@gmail.com>
2020-03-30 22:45:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,14 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+<<<<<<< HEAD
 hash = {:railroads => {:pieces => 4,
   :rent_in_dollars => {},
   :names => {}
+=======
+hash = {:railroads => {
+  :pieces => "4"
+>>>>>>> 1062364f7347609046e5742d81c036386af1af98
 }}
 hash
 end
